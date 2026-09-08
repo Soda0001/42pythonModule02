feb4ce07fa3db5dc6bf69fd040e7686099dac72f
Soda0001 <selcukdeniz239@gmail.com>
2026-09-08 20:02:23 +0300

    i have made a sinful fix

--- ex4/ft_finally_block.py
+++ ex4/ft_finally_block.py
@@ -1,4 +1,40 @@
-from ex3.ft_custom_errors import PlantError
+class GardenError(Exception):
+    def __init__(self, message="Unknown garden error"):
+        super().__init__(message)
+
+
+class PlantError(GardenError):
+    def __init__(self, message="Unknown plant error"):
+        super().__init__(message)
+
+
+class WaterError(GardenError):
+    def __init__(self, message="Unknown water error"):
+        super().__init__(message)
+
+
+def show_plant_name(plant_name: str):
+    if plant_name == "":
+        raise PlantError("Plant name cannot be empty")
+
+    if plant_name == "Abbas":
+        raise PlantError("Plant name cannot be Abbas")
+
+    print(f"Plant name is: {plant_name}")
+
+
+def show_water_amount(water_amount: int):
+    max_liters = 200
+
+    if water_amount < 0:
+        raise WaterError("Water amount cannot be negative")
+
+    if water_amount > max_liters:
+        raise WaterError(
+            f"Water storage limit exceeded! Limit is: {max_liters}L"
+        )
+
+    print(water_amount)
 
 
 def water_plant(plant_name: str):
